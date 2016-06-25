06c4250a0cb16f51624735cd94cc885e975093c8
Giovani Ferreira <silva.giovanif@gmail.com>
2016-06-24 22:36:46 -0300

    Terminei as paginas de login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,27 +9,6 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-
   $scope.items = [
     {nome : "test1"},
     {nome : "test2"},
@@ -37,17 +16,6 @@ angular.module('starter.controllers', [])
   ];
 
   $scope.totalPreco = "PRECO TOTAL = 100";
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
 })
 
 .controller('PlaylistsCtrl', function($scope) {
@@ -67,18 +35,26 @@ angular.module('starter.controllers', [])
 .controller('UserCtrl', function($scope, UserService, $ionicPopup, $state){
   $scope.user = {};
 
+  $scope.login = function() {
+    UserService.LoginUser($scope.user).success(function(data) {
+      console.log("Deu Bom Login");
+    }).error(function(data) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Login falhou!',
+        template: 'Verifique suas credenciais!'
+      });
+    });
+  }
+
   $scope.register = function() {
     UserService.RegisterUser($scope.user).success(function(data) {
         //$state.go('tab.dash');
-        console.log("Deu bom");
+        console.log("Deu bom Cadastro");
     }).error(function(data) {
         var alertPopup = $ionicPopup.alert({
-            title: 'Login failed!',
-            template: 'Please check your credentials!'
+            title: 'Cadastro falhou!',
+            template: 'Verifique seus dados!'
         });
     });
   }
 });
-
-  
-  
